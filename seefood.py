e6264cafaaf2e89ea0279b16f9f5432bb965f480
VikasSewani <vikasksewani007@gmail.com>
2022-07-27 17:10:51 +1000

    update

--- seefood.py
+++ seefood.py
@@ -12,4 +12,17 @@ if image_file is not None:
     file_details = {"filename":image_file.name, "filetype":image_file.type,
                     "filesize":image_file.size}
     st.write(file_details)
-    
+
+api_url = 'https://api.api-ninjas.com/v1/imagetotext'
+
+image_file_descriptor = open('/Users/vikassewani/Desktop/thisimage.png', 'rb')
+files = {'image': image_file_descriptor}
+r = requests.post(api_url, files=files, headers={'X-Api-Key': 'hBeHlA2f2g8gkNunbnBDXg==bgAu26luCQTY4LLO'})
+
+full_string =[]
+arr = r.json()
+
+for i in range(0, len(arr)):
+    full_string.append(arr[i]['text'])
+
+st.write(f' '.join(full_string[0:min(10,len(full_string))]))
