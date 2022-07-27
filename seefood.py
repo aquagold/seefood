1c3fe4e17e44f6eacf123c0db600f76d9eaf38b9
VikasSewani <vikasksewani007@gmail.com>
2022-07-27 17:10:24 +1000

    update

--- seefood.py
+++ seefood.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 
 st.subheader("See Food")
+
+image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+
+im_file_path = '/Users/vikassewani/Desktop/thisimage.png'
+
+if image_file is not None:
+    st.image(load_image(image_file),width=250)
+    file_details = {"filename":image_file.name, "filetype":image_file.type,
+                    "filesize":image_file.size}
+    st.write(file_details)
+    
