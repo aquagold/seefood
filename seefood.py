2144acf844d648d8ae7d18c83f28eb911349cdf2
VikasSewani <vikasksewani007@gmail.com>
2022-07-28 12:40:45 +1000

    reading working

--- seefood.py
+++ seefood.py
@@ -1,39 +1,54 @@
 import streamlit as st
+from PIL import Image
+import requests
+import websockets
+import json
+import os
+
 
 def save_uploadedfile(uploadedfile):
+     check_dir = ''.join([os.getcwd(), '/tempDir/'])
+     if (not os.path.isdir(check_dir)):
+         os.mkdir(check_dir)
+    
      with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
          f.write(uploadedfile.getbuffer())
      return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
  
-    
+
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
  
     
 st.subheader("See Food")
-
-"""
 image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
-im_file_path = '/Users/vikassewani/Desktop/thisimage.png'
 
 if image_file is not None:
+    
     st.image(load_image(image_file),width=250)
     file_details = {"filename":image_file.name, "filetype":image_file.type,
                     "filesize":image_file.size}
-    st.write(file_details)
+    #st.write(file_details)
     save_uploadedfile(image_file)
+    
+    ## Extract data
+    image_dir = ''.join([os.getcwd(), '/tempDir/'])
+    image_file_descriptor = open(''.join([image_dir, image_file.name]), 'rb')
 
 
-api_url = 'https://api.api-ninjas.com/v1/imagetotext'
+    api_url = 'https://api.api-ninjas.com/v1/imagetotext'
+    files = {'image': image_file_descriptor}
+    r = requests.post(api_url, files=files, headers={'X-Api-Key': 'hBeHlA2f2g8gkNunbnBDXg==bgAu26luCQTY4LLO'})
 
-image_file_descriptor = open('/Users/vikassewani/Desktop/thisimage.png', 'rb')
-files = {'image': image_file_descriptor}
-r = requests.post(api_url, files=files, headers={'X-Api-Key': 'hBeHlA2f2g8gkNunbnBDXg==bgAu26luCQTY4LLO'})
+    full_string =[]
+    arr = r.json()
 
-full_string =[]
-arr = r.json()
-
-for i in range(0, len(arr)):
-    full_string.append(arr[i]['text'])
-
-st.write(f' '.join(full_string[0:min(10,len(full_string))]))
-"""
+    for i in range(0, len(arr)):
+        full_string.append(arr[i]['text'])
+        
+    st.write(f' '.join(full_string[0:min(10,len(full_string))]))
+    
+    #Call dall e!
+    
